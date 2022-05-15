64b86dcb7a9a11000ed221b1838e877cb38346ef
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2022-05-15 19:20:00 +0200

    Add files via upload

---
+++ info.py
@@ -0,0 +1,42 @@
+from cgi import print_form
+from instagrapi import Client
+import sys
+
+cl = Client()
+cl.login(username='robin_lachand', password='robinou2007')
+account = cl.account_info().dict()
+
+if len(sys.argv) < 2:
+    print('''
+    Help for InstaInfo:
+
+    Usage : python info.py arg[1]
+
+    arg[1] = name of the user you want the Info
+
+    Exemples :
+
+    python info.py github
+    ''')
+    try:
+        print(sys.argv[3])
+    except IndexError:
+        pass
+    quit()
+
+name = str(sys.argv[1])
+
+info = cl.user_info_by_username(name).dict()
+#print(info)
+print('Profil Id: '+info['pk'])
+print('Profil Name: '+info['username'])
+print('Profil Full Name: '+info['full_name'])
+private = str(info['is_private'])
+verified = str(info['is_verified'])
+print('Verified: '+verified)
+print('Profil Private: '+private)
+print('Business:')
+business = str(info['is_business'])
+print('Is Business: '+business)
+email = str(info['public_email'])
+print('Email: '+email)
